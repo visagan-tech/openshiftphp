10d04fddfd4853192d6178fa3418f49ef5e231c9
visagan-tech <73689223+visagan-tech@users.noreply.github.com>
2020-10-30 16:57:42 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html> 
 	<head> 
-	<title>PHP mayank Test</title>
+	<title>PHP Shivagamigugan Test</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
 	</head>
 
 <body> 
 	<h1>PHP 1663Test</h1>
-		<p><b>An Example of now from terminal chanegs PHP in Action</b></p>
+		<p><b>An Example of now from terminal changes PHP in Action</b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION2 </h2> 
